97b2b1c8ba5a50d3f7c9e09966680b1034acf64f
Ali Imran <real.imrano@proton.me>
2023-08-17 15:37:57 +0100

    ticket-5 in progress

--- models/articleModel.js
+++ models/articleModel.js
@@ -17,5 +17,25 @@ exports.fetchArticles = (article_id) => {
 }
 
 exports.fetchAllArticles = () => {
-    //q\
+    // use a sophisticated query to return the article table, excluding body column, and featuring a column_count column, using Count() aggregate function and Group By
+    // once query returns, proceed as normally, passing it back to the controller, sending back a response and handling any errors
+    // since there is no query or parametric input from the client, there, in theory, cant be any client errors, but error handling chain should still take care of 500s (i.e we code wrong)
+    // update endpoints.json once complete
+
+    return db.query(`
+    SELECT
+    articles.*,
+    COUNT(comments.comment_id) AS comment_count
+FROM
+    articles
+LEFT JOIN
+    comments ON articles.article_id = comments.article_id
+GROUP BY
+    articles.article_id
+ORDER BY
+    articles.date_column DESC;
+    `)
+    .then(data=>{
+        console.log(data);
+    })
 }
