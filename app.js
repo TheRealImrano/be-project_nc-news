eccf92e7a9fb8f0e5413d1af9718beb33918e735
Ali Imran <real.imrano@proton.me>
2023-08-28 17:10:12 +0100

    made some edits to error handlers

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
 
 app.use((err, req, res, next) => {
     if (err.status) {
-      res.status(err.status).send({ msg: err.msg });
+      res.status(err.status).send({ msg: err.msg, code: err.status });
     } else next(err);
 });
 
